0f623e287bfe35b52668fdbf2275f21aa42d91c0
stephengtuggy <sgt@stephengtuggy.com>
2015-09-12 07:47:27 -0700

    Initial commit of StephenGTuggy.FuzzyMatch

---
+++ StephenGTuggy.FuzzyMatch/StephenGTuggy.FuzzyMatch/IStringSimilarity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StephenGTuggy.FuzzyMatch
+{
+    public interface IStringSimilarity
+    {
+        float GetSimilarity(string p_ValueA, string p_ValueB);
+    }
+}
