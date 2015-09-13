51770a27457f803ccd52ee0901ce01c40a082cfb
stephengtuggy <sgt@stephengtuggy.com>
2015-09-13 12:14:30 -0700

    Added DoubleMetaphoneSimilarityCalc to allow calculating the similarity between two DoubleMetaphone phonetic representations, not just between the words that they represent.

---
+++ StephenGTuggy.FuzzyMatch/StephenGTuggy.FuzzyMatch/DoubleMetaphoneSimilarityCalc.cs
@@ -0,0 +1,44 @@
+﻿using nullpointer.Metaphone;
+
+namespace StephenGTuggy.FuzzyMatch
+{
+    public class DoubleMetaphoneSimilarityCalc : ISimilarityCalc<DoubleMetaphone>
+    {
+        public const float PRIMARY_KEYS_EQUAL = 0.98F;
+        public const float PRIMARY_KEY_EQUALS_ALTERNATE = 0.90F;
+        public const float ALTERNATE_KEYS_EQUAL = 0.80F;
+
+        public float CalcSimilarity(DoubleMetaphone p_ValueA, DoubleMetaphone p_ValueB)
+        {
+            if ((p_ValueA.PrimaryKey == p_ValueB.PrimaryKey)
+                && (p_ValueA.AlternateKey == p_ValueB.AlternateKey))
+            {
+                return SimilarityCalc.MAX_POSSIBLE_SIMILARITY;
+            }
+            else if (p_ValueA.PrimaryKey.Equals(p_ValueB.PrimaryKey))
+            {
+                return PRIMARY_KEYS_EQUAL;
+            }
+            else if (!string.IsNullOrEmpty(p_ValueB.AlternateKey)
+                && p_ValueA.PrimaryKey.Equals(p_ValueB.AlternateKey))
+            {
+                return PRIMARY_KEY_EQUALS_ALTERNATE;
+            }
+            else if (!string.IsNullOrEmpty(p_ValueA.AlternateKey)
+                && p_ValueB.PrimaryKey.Equals(p_ValueA.AlternateKey))
+            {
+                return PRIMARY_KEY_EQUALS_ALTERNATE;
+            }
+            else if (!string.IsNullOrEmpty(p_ValueA.AlternateKey)
+                && !string.IsNullOrEmpty(p_ValueB.AlternateKey)
+                && p_ValueA.AlternateKey.Equals(p_ValueA.AlternateKey))
+            {
+                return ALTERNATE_KEYS_EQUAL;
+            }
+            else
+            {
+                return SimilarityCalc.MIN_POSSIBLE_SIMILARITY;
+            }
+        }
+    }
+}
--- StephenGTuggy.FuzzyMatch/StephenGTuggy.FuzzyMatch/WordSimilarityCalcDoubleMetaphone.cs
+++ StephenGTuggy.FuzzyMatch/StephenGTuggy.FuzzyMatch/WordSimilarityCalcDoubleMetaphone.cs
@@ -25,9 +25,7 @@ namespace StephenGTuggy.FuzzyMatch
 {
     public class WordSimilarityCalcDoubleMetaphone : IStringSimilarityCalc
     {
-        public const float PRIMARY_KEYS_EQUAL = 0.98F;
-        public const float PRIMARY_KEY_EQUALS_ALTERNATE = 0.90F;
-        public const float ALTERNATE_KEYS_EQUAL = 0.80F;
+        private DoubleMetaphoneSimilarityCalc _DoubleMetaphoneSimilarityCalc = new DoubleMetaphoneSimilarityCalc();
         
         public float CalcSimilarity(string p_ValueA, string p_ValueB)
         {
@@ -38,31 +36,7 @@ namespace StephenGTuggy.FuzzyMatch
             
             DoubleMetaphone l_DoubleMetaphoneA = new DoubleMetaphone(p_ValueA);
             DoubleMetaphone l_DoubleMetaphoneB = new DoubleMetaphone(p_ValueB);
-            
-            if (l_DoubleMetaphoneA.PrimaryKey.Equals(l_DoubleMetaphoneB.PrimaryKey))
-            {
-                return PRIMARY_KEYS_EQUAL;
-            }
-            else if (!string.IsNullOrEmpty(l_DoubleMetaphoneB.AlternateKey)
-                && l_DoubleMetaphoneA.PrimaryKey.Equals(l_DoubleMetaphoneB.AlternateKey))
-            {
-                return PRIMARY_KEY_EQUALS_ALTERNATE;
-            }
-            else if (!string.IsNullOrEmpty(l_DoubleMetaphoneA.AlternateKey)
-                && l_DoubleMetaphoneB.PrimaryKey.Equals(l_DoubleMetaphoneA.AlternateKey))
-            {
-                return PRIMARY_KEY_EQUALS_ALTERNATE;
-            }
-            else if (!string.IsNullOrEmpty(l_DoubleMetaphoneA.AlternateKey)
-                && !string.IsNullOrEmpty(l_DoubleMetaphoneB.AlternateKey)
-                && l_DoubleMetaphoneA.AlternateKey.Equals(l_DoubleMetaphoneA.AlternateKey))
-            {
-                return ALTERNATE_KEYS_EQUAL;
-            }
-            else
-            {
-                return SimilarityCalc.MIN_POSSIBLE_SIMILARITY;
-            }
+            return _DoubleMetaphoneSimilarityCalc.CalcSimilarity(l_DoubleMetaphoneA, l_DoubleMetaphoneB);
         }
     }
 }
